d6c7dd81eea717a12eb68c7a535ec8f500ea3053
hemiahwu <309595700@qq.com>
2022-06-18 14:39:59 +0800

    03login

--- src/router/index.js
+++ src/router/index.js
@@ -6,11 +6,26 @@ const routes = [
     name: "Index",
     component: () => import("../views/Index.vue"),
   },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+// 路由守卫
+router.beforeEach((to, from, next) => {
+  const isLogin = localStorage.ele_login ? true : false;
+  if (to.path === "/login") {
+    next();
+  } else {
+    isLogin ? next() : next("/login");
+  }
 });
 
 export default router;
---
+++ src/views/Login.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="login">
+    <div class="logo">
+      <img src="../assets/logo.jpg" alt="my login image" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.login {
+  width: 100%;
+  height: 100%;
+  padding: 30px;
+  box-sizing: border-box;
+  background: #fff;
+}
+
+.logo {
+  text-align: center;
+}
+
+.logo img {
+  width: 150px;
+}
+</style>
