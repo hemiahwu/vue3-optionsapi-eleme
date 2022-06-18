001836e68450c307df80d9640a1cbe65a4517758
hemiahwu <309595700@qq.com>
2022-06-18 22:12:42 +0800

    05封装组件

---
+++ src/components/InputGroup.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="text-group">
+    <div class="input-group">
+      <input :type="type" :value="value" :placeholder="placeholder" />
+      <button v-if="btnTitle" :disabled="disabled" @click="$emit('btnClick')">
+        {{ btnTitle }}
+      </button>
+    </div>
+    <div v-if="error" class="invalid-feedback">{{ error }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+    },
+    value: String,
+    placeholder: String,
+    btnTitle: String,
+    disabled: Boolean,
+    error: String,
+  },
+};
+</script>
+
+<style scoped>
+/* 封装表单 */
+.input-group {
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+.input-group input {
+  height: 100%;
+  width: 60%;
+  outline: none;
+}
+.input-group button {
+  border: none;
+  outline: none;
+  background: #fff;
+}
+.input-group button[disabled] {
+  color: #aaa;
+}
+.is-invalid {
+  border: 1px solid red;
+}
+.invalid-feedback {
+  color: red;
+  padding-top: 5px;
+}
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,20 +3,21 @@
     <div class="logo">
       <img src="../assets/logo.jpg" alt="my login image" />
     </div>
-
-    <div class="text-group">
-      <div class="input-group">
-        <input type="text" value="" placeholder="手机号" />
-        <button>获取验证码</button>
-      </div>
-    </div>
-
-    <div class="text-group">
-      <div class="input-group">
-        <input type="text" value="" placeholder="验证码" />
-      </div>
-    </div>
-    <div v-if="false" class="invalid-feedback">验证码有误</div>
+    <InputGroup
+      type="number"
+      v-model="phone"
+      placeholder="手机号"
+      :btnTitle="btnTitle"
+      :disabled="disabled"
+      :error="errors.phone"
+      @btnClick="getVerifyCode"
+    />
+    <InputGroup
+      type="number"
+      v-model="verifyCode"
+      placeholder="验证码"
+      :error="errors.code"
+    />
 
     <div class="login-des">
       <p>新用户登录即自动注册，表示已同意 <span>《用户服务协议》</span></p>
@@ -29,7 +30,25 @@
 </template>
 
 <script>
-export default {};
+import InputGroup from "../components/InputGroup.vue";
+export default {
+  name: "Login",
+  components: { InputGroup },
+  data() {
+    return {
+      phone: "",
+      btnTitle: "获取验证码",
+      disabled: false,
+      errors: {},
+      verifyCode: "",
+    };
+  },
+  methods: {
+    getVerifyCode() {
+      console.log("获取验证码");
+    },
+  },
+};
 </script>
 
 <style>
@@ -74,35 +93,5 @@ export default {};
 }
 .login-btn button[disabled] {
   background-color: #8bda81;
-}
-
-/* 封装表单 */
-.input-group {
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  padding: 10px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-}
-.input-group input {
-  height: 100%;
-  width: 60%;
-  outline: none;
-}
-.input-group button {
-  border: none;
-  outline: none;
-  background: #fff;
-}
-.input-group button[disabled] {
-  color: #aaa;
-}
-.is-invalid {
-  border: 1px solid red;
-}
-.invalid-feedback {
-  color: red;
-  padding-top: 5px;
 }
 </style>
