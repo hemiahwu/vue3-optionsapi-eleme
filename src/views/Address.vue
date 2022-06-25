a3bb5b9349ea3bab28d785de08cf5a199669cd9e
hemiahwu <309595700@qq.com>
2022-06-25 22:06:05 +0800

    13

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -1,13 +1,47 @@
 <template>
   <div class="addresss">
     <Header :isLeft="true" title="选择收货地址" />
+    <div class="city-search">
+      <div class="search">
+        <span class="city">
+          {{ city }}
+          <i class="fa fa-angle-down"></i>
+        </span>
+        <i class="fa fa-search"></i>
+        <input
+          type="text"
+          v-model="searchValue"
+          placeholder="小区/写字楼/学校等"
+        />
+      </div>
+      <!-- 当前定位 -->
+      <Location :address="address" />
+    </div>
   </div>
 </template>
 
 <script>
 import Header from "../components/Header.vue";
+import Location from "../components/Location.vue";
 export default {
-  components: { Header },
+  data() {
+    return {
+      city: "",
+      searchValue: "",
+    };
+  },
+  computed: {
+    address() {
+      return this.$store.getters.address;
+    },
+  },
+  components: { Header, Location },
+  beforeRouteEnter(to, from, next) {
+    // console.log(to);
+    next((vm) => {
+      vm.city = to.params.city;
+    });
+  },
 };
 </script>
 
@@ -18,5 +52,31 @@ export default {
   overflow: auto;
   box-sizing: border-box;
   padding-top: 45px;
+}
+
+.city-search {
+  background-color: #fff;
+  padding: 10px 20px;
+  color: #333;
+  margin-top: 45px;
+}
+.search {
+  background-color: #eee;
+  height: 40px;
+  border-radius: 10px;
+  box-sizing: border-box;
+  line-height: 40px;
+}
+.search .city {
+  padding: 0 10px;
+}
+.city i {
+  margin-right: 10px;
+}
+.search input {
+  margin-left: 5px;
+  background-color: #eee;
+  outline: none;
+  border: none;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="home">
     <div class="header">
-      <div class="address-map" @click="$router.push('/address')">
+      <div
+        class="address-map"
+        @click="$router.push({ name: 'Address', params: { city: city } })"
+      >
         <i class="fa fa-map-marker"></i>
         <span>{{ address }}</span>
         <i class="fa fa-sort-desc"></i>
@@ -20,6 +23,9 @@ export default {
   computed: {
     address() {
       return this.$store.getters.address;
+    },
+    city() {
+      return this.$store.getters.location.addressComponent.city;
     },
   },
 };
