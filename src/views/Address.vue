be1bf9794f3791a665beb2dc24280bd3dff9cb63
hemiahwu <309595700@qq.com>
2022-06-27 17:20:43 +0800

    14

--- src/views/Address.vue
+++ src/views/Address.vue
@@ -17,6 +17,13 @@
       <!-- 当前定位 -->
       <Location :address="address" />
     </div>
+    <!-- 列表 -->
+    <ul class="area">
+      <li v-for="(item, index) in areaList" :key="index">
+        <h4>{{ item.name }}</h4>
+        <p>{{ item.district }} {{ item.address }}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -28,11 +35,36 @@ export default {
     return {
       city: "",
       searchValue: "",
+      areaList: [],
     };
   },
   computed: {
     address() {
       return this.$store.getters.address;
+    },
+  },
+  watch: {
+    searchValue() {
+      this.handleSearch();
+    },
+  },
+  methods: {
+    handleSearch() {
+      // console.log(this.searchValue);
+      const self = this;
+      AMap.plugin("AMap.AutoComplete", function () {
+        var autoOptions = {
+          //city 限定城市，默认全国
+          city: self.city,
+        };
+        // 实例化AutoComplete
+        var autoComplete = new AMap.AutoComplete(autoOptions);
+        // 根据关键字进行搜索
+        autoComplete.search(self.searchValue, function (status, result) {
+          // 搜索成功时，result即是对应的匹配数据
+          self.areaList = result.tips;
+        });
+      });
     },
   },
   components: { Header, Location },
@@ -78,5 +110,20 @@ export default {
   background-color: #eee;
   outline: none;
   border: none;
+}
+
+.area {
+  margin-top: 16px;
+  background: #fff;
+}
+.area li {
+  border-bottom: 1px solid #eee;
+  padding: 8px 16px;
+  color: #aaa;
+}
+.area li h4 {
+  font-weight: bold;
+  color: #333;
+  margin-bottom: 5px;
 }
 </style>
